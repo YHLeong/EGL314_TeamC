a9c5c710b9c675dc173a6191b8fce568df90458a
Yu Hang <leongyuhang794@gmail.com>
2025-07-02 21:16:51 +0800

    Update Reaper.py
    added delay and removed play/stop toggle

--- Backlog 3 sprint 1/Reaper.py
+++ Backlog 3 sprint 1/Reaper.py
@@ -1,4 +1,5 @@
 from pythonosc import udp_client
+import time
 
 def send_message(receiver_ip, receiver_port, address, message):
 	try:
@@ -12,7 +13,14 @@ def send_message(receiver_ip, receiver_port, address, message):
 REAPER_IP = "192.168.254.60"  # IP of the machine running REAPER
 PORT = 6800
 
-# Examples
-send_message(REAPER_IP, PORT, "/action/40044", 1.0)  # Play/Stop
-send_message(REAPER_IP, PORT, "/action/1007", 1.0)   # Stop
+
+# PLAY
 send_message(REAPER_IP, PORT, "/action/40073", 1.0)  # Play
+time.sleep(3)
+
+# STOP
+send_message(REAPER_IP, PORT, "/action/1007", 1.0)   # Stop
+time.sleep(3)
+
+# TOGGLE (Play/Stop Toggle)
+send_message(REAPER_IP, PORT, "/action/40044", 1.0)  # Play/Stop Toggle
