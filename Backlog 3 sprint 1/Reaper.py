57ff5d0c4cc8f8744bcf685d392e817cf8eb9443
Yu Hang <leongyuhang794@gmail.com>
2025-07-04 08:58:03 +0800

    Update Reaper.py

--- Backlog 3 sprint 1/Reaper.py
+++ Backlog 3 sprint 1/Reaper.py
@@ -13,7 +13,6 @@ def send_message(receiver_ip, receiver_port, address, message):
 REAPER_IP = "192.168.254.60"  # IP of the machine running REAPER
 PORT = 6800
 
-
 # PLAY
 send_message(REAPER_IP, PORT, "/action/40073", 1.0)  # Play
 time.sleep(3)
