bc5cdb7fa7baea1e382f53e9a86fe192fc7e8af9
Yu Hang <leongyuhang794@gmail.com>
2025-07-10 10:32:55 +0800

    Update Reaper_Marker.py

--- Backlog 3 Sprint 2/Reaper_Marker.py
+++ Backlog 3 Sprint 2/Reaper_Marker.py
@@ -1,4 +1,5 @@
 from pythonosc import udp_client
+import time
 
 def send_message(receiver_ip, receiver_port, address, message):
 	try:
@@ -16,12 +17,14 @@ def send_message(receiver_ip, receiver_port, address, message):
 PI_A_ADDR = "192.168.254.12"	# wlan ip
 PORT = 8000
 
-addr= "/action/41261" # Jump to Marker 21
-addr = "/action/41262" # Jump to Marker 22
+addr= "/action/41261" 	# Jump to Marker 21
 addr1 = "/action/1007"  # Play
-msg = float(1) # Trigger TRUE Value
+addr2 = "/action/1016"  # Stop
+msg = float(1) 			# Trigger TRUE Value
 
-send_message(PI_A_ADDR, PORT, addr, msg)  # Jump to marker
-send_message(PI_A_ADDR, PORT, addr1, msg)  # Play
+send_message(PI_A_ADDR, PORT, addr, msg)  	# Jump to marker
+send_message(PI_A_ADDR, PORT, addr1, msg)  	# Play
+time.sleep(5)  								# Wait for 5 second to allow the play action to take effect
+send_message(PI_A_ADDR, PORT, addr2, msg)  	# Stop
 
 
